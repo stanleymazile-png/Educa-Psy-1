7073ffb2f96d7822310decf2f4e49381e2d052b2
stanleymazile-png <stanleymazile@gmail.com>
2026-01-02 23:14:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ window.onclick = function() {
     menu.classList.remove('voir');
 }
 
+// Détecte le changement dans votre menu et ordonne la traduction intégrale
+document.getElementById('select-langue').addEventListener('change', function() {
+    var lang = this.value;
+    var googleCombo = document.querySelector('.goog-te-combo');
+    
+    if (googleCombo) {
+        googleCombo.value = lang;
+        googleCombo.dispatchEvent(new Event('change'));
+    }
+});
+
 
 
 
