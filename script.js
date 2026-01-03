79aefa28ceff253cef594aca5216b9294166eb2b
stanleymazile-png <stanleymazile@gmail.com>
2026-01-02 21:37:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const btnMenu = document.getElementById('btnMenu');
-const liens = document.getElementById('liens-deroulants');
+const menu = document.getElementById('liens-deroulants');
 
-// Gérer l'ouverture du menu
 btnMenu.onclick = function(e) {
-    liens.classList.toggle('voir');
+    menu.classList.toggle('voir');
     e.stopPropagation();
-};
+}
 
-// Fermer le menu si on clique ailleurs sur la page
 window.onclick = function() {
-    liens.classList.remove('voir');
-};
+    menu.classList.remove('voir');
+}
+
+
 
 
 
